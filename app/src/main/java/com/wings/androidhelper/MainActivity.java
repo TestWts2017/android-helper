15ca5b74dc806b889908bd8350080b47f1e923c0
NikunjD <nikunj.d@wingstechsolutions.com>
2019-06-11 18:51:46 +0530

    Image Compress technique helper included

--- app/src/main/java/com/wings/androidhelper/MainActivity.java
+++ app/src/main/java/com/wings/androidhelper/MainActivity.java
@@ -2,6 +2,8 @@ package com.wings.androidhelper;
 
 import android.os.Bundle;
 
+import com.wings.helper.CompressImageHelper;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        String path = "/storage/emulated/0/Pictures/1556621253166.jpg";
+//        CompressImageHelper.compressImage(path, 500, 500);
     }
 }
---
+++ helper/src/main/java/com/wings/helper/CompressImageHelper.java
@@ -0,0 +1,214 @@
+package com.wings.helper;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ * Purpose: compress image resolutions and size
+ *
+ * @author NikunjD
+ * Created on June 11, 2019
+ * Modified on June 11, 2019
+ */
+public class CompressImageHelper {
+
+
+    /**
+     * Get actual path from URI
+     *
+     * @param context    context
+     * @param contentUri image uri
+     * @return path of image uri
+     */
+    public static String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = {MediaStore.Images.Media.DATA};
+            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+            if (cursor != null) {
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                cursor.moveToFirst();
+                return cursor.getString(column_index);
+            } else {
+                return "";
+            }
+        } catch (Exception e) {
+            Log.e("TAG", "getRealPathFromURI Exception : " + e.toString());
+            return "";
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+
+    /**
+     * Compress image with aspect ratio
+     *
+     * @param imagePath image path
+     * @param maxHeight maximum require height in resolution
+     * @param maxWidth  maximum require width in resolution
+     * @return compressed image
+     */
+    public static byte[] compressImage(String imagePath, float maxHeight, float maxWidth) {
+        Bitmap scaledBitmap = null;
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        Bitmap bmp = BitmapFactory.decodeFile(imagePath, options);
+
+        int actualHeight = options.outHeight;
+        int actualWidth = options.outWidth;
+        float imgRatio = (float) actualWidth / (float) actualHeight;
+        float maxRatio = maxWidth / maxHeight;
+
+        if (actualHeight > maxHeight || actualWidth > maxWidth) {
+            if (imgRatio < maxRatio) {
+                imgRatio = maxHeight / actualHeight;
+                actualWidth = (int) (imgRatio * actualWidth);
+                actualHeight = (int) maxHeight;
+            } else if (imgRatio > maxRatio) {
+                imgRatio = maxWidth / actualWidth;
+                actualHeight = (int) (imgRatio * actualHeight);
+                actualWidth = (int) maxWidth;
+            } else {
+                actualHeight = (int) maxHeight;
+                actualWidth = (int) maxWidth;
+
+            }
+        }
+
+        options.inSampleSize = calculateInSampleSize(options, actualWidth, actualHeight);
+        options.inJustDecodeBounds = false;
+        options.inDither = false;
+        options.inPurgeable = true;
+        options.inInputShareable = true;
+        options.inTempStorage = new byte[16 * 1024];
+
+        try {
+            bmp = BitmapFactory.decodeFile(imagePath, options);
+        } catch (OutOfMemoryError exception) {
+            exception.printStackTrace();
+
+        }
+        try {
+            scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight, Bitmap.Config.ARGB_8888);
+        } catch (OutOfMemoryError exception) {
+            exception.printStackTrace();
+        }
+
+        float ratioX = actualWidth / (float) options.outWidth;
+        float ratioY = actualHeight / (float) options.outHeight;
+        float middleX = actualWidth / 2.0f;
+        float middleY = actualHeight / 2.0f;
+
+        Matrix scaleMatrix = new Matrix();
+        scaleMatrix.setScale(ratioX, ratioY, middleX, middleY);
+
+        Canvas canvas = new Canvas(scaledBitmap);
+        canvas.setMatrix(scaleMatrix);
+        canvas.drawBitmap(bmp, middleX - bmp.getWidth() / 2, middleY - bmp.getHeight() / 2, new Paint(Paint.FILTER_BITMAP_FLAG));
+
+        ExifInterface exif;
+        try {
+            exif = new ExifInterface(imagePath);
+            int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 0);
+            Matrix matrix = new Matrix();
+            if (orientation == 6) {
+                matrix.postRotate(90);
+            } else if (orientation == 3) {
+                matrix.postRotate(180);
+            } else if (orientation == 8) {
+                matrix.postRotate(270);
+            }
+            scaledBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0, scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 80, out);
+        return out.toByteArray();
+    }
+
+
+    /**
+     * Calculate sample size value
+     * For more info refer @link https://developer.android.com/topic/performance/graphics/load-bitmap
+     *
+     * @param options   as bitmap
+     * @param reqWidth  require image width
+     * @param reqHeight require image height
+     * @return sample size value
+     */
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+        final float totalPixels = width * height;
+        final float totalReqPixelsCap = reqWidth * reqHeight * 2;
+
+        while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+            inSampleSize++;
+        }
+
+        return inSampleSize;
+    }
+
+
+    /**
+     * Scale Bitmap
+     *
+     * @param context          context
+     * @param selectedImageUri selected image uri
+     * @param croppedPicWidth  The new bitmap's desired width.
+     * @param croppedPicHeight The new bitmap's desired height.
+     * @param imageQuality     image quality
+     * @return The new scaled bitmap
+     */
+    public static byte[] compressImageWithScaling(Context context, Uri selectedImageUri, int croppedPicWidth, int croppedPicHeight, int imageQuality) {
+        byte[] byteArray = null;
+        Bitmap bitmap = null;
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), selectedImageUri);
+            bitmap = Bitmap.createScaledBitmap(bitmap, croppedPicWidth, croppedPicHeight, true);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, imageQuality, outStream); // bmp is your Bitmap instance
+            byteArray = outStream.toByteArray();
+            bitmap.recycle();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (outStream != null) {
+                    outStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return byteArray;
+    }
+
+}
